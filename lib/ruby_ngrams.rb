5d587ca8fa2471992b7c5ad2918dca0822c944f1
Martin Velez <mvelez999@gmail.com>
2011-10-22 00:10:54 -0700

    decided to change Ngrams to a module instead of a class.  next: add cli application to bin dir

--- lib/ruby_ngrams.rb
+++ lib/ruby_ngrams.rb
@@ -1,26 +1,13 @@
-class Ngram
-	include Enumerable
+module Ngrams
 
-	attr_reader :ngrams
-	def initialize(string, n, unit)
-		@string = string
-		@n = n
-		@unit = unit
-		@ngrams = []
-		generate
-	end
-
-	def generate  
-		max_pos = @string.length - @n 
+	def self.extract(string, n, unit) 
+		ngrams = []
+		max_pos = string.length - n 
 		for i in 0..max_pos
-			@ngrams.push(@string[i..i+(@n-1)])	
+			ngrams.push(string[i..i+(n-1)])	
 		end
+		ngrams
 	end	
 
-	def each
-		@ngrams.each do |ngram|
-			yield ngram
-		end	
-	end
-end
-
+	#module_function :extract
+end #module
